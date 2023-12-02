7662217df48e20e3a457de1bedd94b77011cf93e
Mỹ Duyên <130841512+Mint234556@users.noreply.github.com>
2023-12-02 21:15:48 +0700

    commit

--- src/server.js
+++ src/server.js
@@ -2,12 +2,16 @@ import express from "express";
 import configviewEngine from "./config/viewEngine";
 import initWebRoutes from "./routes/web";
 require("dotenv").config();
-
+import bodyParser from "body-parser";
 let app = express();
 
 //config app
 
 configviewEngine(app);
+
+//config body-parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 initWebRoutes(app);
 
